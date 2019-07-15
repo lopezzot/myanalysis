dea8beebc05827ab33dfa0c2284c6e248a3efe7e
lopezzot <lorenzo.pezzotti01@gmail.com>
2019-07-15 17:52:11 +0200

    t240

--- exclusionplot.py
+++ exclusionplot.py
@@ -24,6 +24,7 @@ m365 = [50, 50, 100, 150, 200, 240, 300, 360, 360]
 
 m240 = [10000, 100, 150, 180, 220, 235, 235]
 c240 = [1000, 0.122954, 0.156492, 0.271, 1.5352, 13.147, 10000]
+t240 = [1000, 0.008302866109567658, 0.014043267960524476, 0.02400387934037744, 0.1135393636633489, 0.8767545798463641, 10000]
 
 t365 = [1000, 0.011805359356280826, 0.01291984002946909, 0.016237318278912834, 0.022378778938076393, 0.03174363246917037, 0.07593830445852476, 3.1646458992183386, 1000]
 
@@ -46,6 +47,8 @@ for t in c240:
 	coupling240.append(t)
 for d in m240:
 	masses240.append(d)
+for i in t240:
+	theoretical240.append(i)
 
 canvas=TCanvas("canvas","canvas",800,800)
 canvas.SetLogy()
@@ -128,6 +131,24 @@ Exclusiongrapht365.SetLineWidth(3)
 Exclusiongrapht365.SetLineStyle(3)
 Exclusiongrapht365.SetFillStyle(3001)
 
+Exclusiongrapht240 = TGraph(len(masses240), masses240, theoretical240)
+Exclusiongrapht240.SetTitle("")
+Exclusiongrapht240.GetHistogram().SetMaximum(2000.)
+Exclusiongrapht240.GetHistogram().SetMinimum(0.1e-6)
+XAxis = Exclusiongrapht240.GetXaxis()
+XAxis.SetLimits(1.e-15,2000)
+XAxis.SetNdivisions(105)
+XAxis.SetTitle("M_{a} [GeV]")
+XAxis.SetTitleOffset(1.5)
+YAxis = Exclusiongrapht240.GetYaxis()
+YAxis.SetNdivisions(105)
+YAxis.SetTitle("c")
+Exclusiongrapht240.SetFillColorAlpha(5, 0.5)
+Exclusiongrapht240.SetLineColor(5)
+Exclusiongrapht240.SetLineWidth(5)
+Exclusiongrapht240.SetLineStyle(5)
+Exclusiongrapht240.SetFillStyle(3001)
+
 '''
 Exclusiongraph365.Draw("afl")
 Exclusiongraph2.Draw("same afl")
@@ -141,6 +162,7 @@ mg.Add(Exclusiongraph2)
 mg.Add(Exclusiongraph365)
 mg.Add(Exclusiongrapht365)
 mg.Add(Exclusiongraph240)
+mg.Add(Exclusiongrapht240)
 mg.SetTitle("")
 mg.GetHistogram().SetMaximum(2000.)
 mg.GetHistogram().SetMinimum(0.1e-6)
